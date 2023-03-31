99d6a0d39fc6b1faa8f22f8fe58fef097c92434e
Yangyang <ys.yang@55688.com.tw>
2023-03-31 17:22:31 +0800

    完成 detail page UI ＆ 排版變化

--- flutterweek1/lib/main.dart
+++ flutterweek1/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'vertical_view.dart';
 import 'horizontal_view.dart';
@@ -26,73 +25,68 @@ class Stylish extends StatelessWidget {
 List<Widget> createRowContainer() {
   List<Widget> containers = [];
   for (int i = 0; i < 10; i++) {
-    containers.add(
-      Container(
-        height: 160,
-        width: 250,
-        padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-        child: FittedBox(
-          fit: BoxFit.fill,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: Image.network(
-              'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2016/03/28/20160328-024000_U6251_M142129_6df2.jpg?itok=4KQSoi0q'
-              ),
-          ),
+    containers.add(Container(
+      height: 160,
+      width: 250,
+      padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.network(
+              'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2016/03/28/20160328-024000_U6251_M142129_6df2.jpg?itok=4KQSoi0q'),
         ),
-      )
-    );
+      ),
+    ));
     containers.add(
-      const SizedBox(height: 5,),
+      const SizedBox(
+        height: 5,
+      ),
     );
   }
   return containers;
 }
 
 List<Widget> createContainerWithPic() {
-  List<Widget>containers = [];
+  List<Widget> containers = [];
   for (int i = 0; i < 5; i++) {
     containers.add(
-        Container(
-          decoration: BoxDecoration(
+      Container(
+        decoration: BoxDecoration(
           border: Border.all(),
           borderRadius: const BorderRadius.all(Radius.circular(10)),
-             ),
-          height: 100,
-          child: Row(
-            children: [
-              SizedBox(
-                height: 130,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10), 
-                    bottomLeft: Radius.circular(10)
-                    ),
-                  child: FittedBox(
-                    fit: BoxFit.fill,
-                    child: Image.network(
-                    'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2016/03/28/20160328-024000_U6251_M142129_6df2.jpg?itok=4KQSoi0q'
-                     ),
-                    ),
-                  ),
-                ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('1231233'),
-                  Text('4645644')
-                ],
-              )
-            ],
-          ),
         ),
+        height: 100,
+        child: Row(
+          children: [
+            SizedBox(
+              height: 130,
+              width: 100,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10)),
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Image.network(
+                      'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2016/03/28/20160328-024000_U6251_M142129_6df2.jpg?itok=4KQSoi0q'),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [Text('1231233'), Text('4645644')],
+            )
+          ],
+        ),
+      ),
       // ),
     );
     containers.add(
-      const SizedBox(height: 5,),
+      const SizedBox(
+        height: 5,
+      ),
     );
   }
   return containers;
 }
-
--- flutterweek1/lib/second_page.dart
+++ flutterweek1/lib/second_page.dart
@@ -3,99 +3,301 @@ import 'package:flutterweek1/data_model.dart';
 import 'data_model.dart';
 import 'underline_view.dart';
 
-class SecondPage extends StatelessWidget {
-  
+class SecondPage extends StatefulWidget {
   final itemData item;
-  const SecondPage({ required this.item });
+
+  const SecondPage({required this.item});
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.white,
-      title: Container(
-        height: 70,
-        width: 200,
-        child: Image.network(
-          'https://cdn.discordapp.com/attachments/1083197828467265564/1087723351893610516/Image_Logo02.png',
-          height: 100,
-          width: 100,
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  int selectedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Container(
+          height: 70,
+          width: 200,
+          child: Image.network(
+            'https://cdn.discordapp.com/attachments/1083197828467265564/1087723351893610516/Image_Logo02.png',
+            height: 100,
+            width: 100,
+          ),
         ),
       ),
-    ),
-    body: SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 400,
-                  width: 300,
-                  child: FittedBox(
-                    fit: BoxFit.fill,
-                    child: Image.network(
-                      item.image
-                      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  size.width >= 600
+                      ? Container(
+                          height: 400,
+                          width: 300,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Image.network(widget.item.image),
+                          ),
+                        )
+                      : SizedBox.shrink(),
+                  size.width < 600
+                      ? Container(
+                          height: 400,
+                          width: 350,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Image.network(widget.item.image),
+                          ),
+                        )
+                      : SizedBox.shrink(),
+                  SizedBox(
+                    width: 15,
                   ),
-                ),
-                SizedBox(width: 15,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:  [
-                      Text('Uniqlo 特級輕羽絨', 
-                      style: TextStyle(
-                        fontSize: 22
-                        ),
+                  size.width >= 600
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: createTopDetailColum(widget.item),
+                        )
+                      : SizedBox.shrink(),
+                ],
+              ),
+              size.width < 600
+                  ? Container(
+                      width: 350,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: createTopDetailColum(widget.item),
                       ),
-                      Text(item.name),
-                      SizedBox(height: 10,),
-                      Text('NT\$ ' + (item.price).toString()),
-                      HorizontalLine(dashedWidth: 200, dashedHeight: 1, color: Color.fromARGB(255, 160, 164, 166),),
-                      Row(
-                        children: [
-                          Text('顏色｜'),
-                          TextButton(
-                            style: TextButton.styleFrom(backgroundColor: Colors.green),
-                            onPressed: () {
-                              print('tappp');
-                            }, 
-                            child: Text('')),
-                        ],
-                      )
-                    ],
-                  )
-              ],
-            ),
-            SizedBox(height: 20,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('123123123'),
-                    HorizontalLine(dashedWidth: 450, dashedHeight: 1, color: Colors.black,),
-                  ],
-                ),
-                Container(
-                  width: 520,
-                  child: Text(
-                    '測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字'
-                    ),
-                ),
-                SizedBox(height: 15,),
-                Image.network(item.image, height: 330, width: 520, fit: BoxFit.fill,),
-                SizedBox(height: 15,),
-                Image.network(item.image, height: 330, width: 520, fit: BoxFit.fill,),
-              ],
-            )
-          ],
+                    )
+                  : SizedBox.shrink(),
+              SizedBox(
+                height: 20,
+              ),
+              size.width >= 600
+                  ? Container(
+                      width: 570,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: createBottomDetailColum(),
+                      ),
+                    )
+                  : SizedBox.shrink(),
+              size.width < 600
+                  ? Container(
+                      width: 350,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: createBottomDetailColum(),
+                      ),
+                    )
+                  : SizedBox.shrink(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+//Function
+  SizedBox buildBtnwithText(int index, String text) {
+    return SizedBox(
+      height: 25,
+      width: 40,
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: selectedIndex == index ? Colors.blue : Colors.grey,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18))),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 10),
+        ),
+      ),
+    );
+  }
+
+  Image createPicBox(String url) {
+    return Image.network(
+      url,
+      width: 570,
+      height: 330,
+      fit: BoxFit.fill,
+    );
+  }
+
+  List<Widget> createTopDetailColum(itemData item) {
+    return [
+      Text(
+        'Uniqlo 特級輕羽絨',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+      ),
+      Text(item.name),
+      SizedBox(
+        height: 15,
+      ),
+      Text(
+        'NT\$ ' + (item.price).toString(),
+        style: TextStyle(fontSize: 18),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      HorizontalLine(
+        dashedWidth: 250,
+        dashedHeight: 1,
+        color: Color.fromARGB(255, 160, 164, 166),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Row(
+        children: [
+          Text('顏色｜'),
+          buildBtnWithColorBox(Colors.green),
+          SizedBox(
+            width: 5,
+          ),
+          buildBtnWithColorBox(Colors.blue)
+        ],
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Row(
+        children: [
+          Text('尺寸｜'),
+          buildBtnwithText(0, '1'),
+          SizedBox(
+            width: 5,
+          ),
+          buildBtnwithText(1, '2'),
+          SizedBox(
+            width: 5,
+          ),
+          buildBtnwithText(2, '3'),
+        ],
+      ),
+      SizedBox(
+        height: 25,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('數量｜')],
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      SizedBox(
+        height: 45,
+        width: 260,
+        child: TextButton(
+          onPressed: () {},
+          child: Text(
+            '請選擇尺寸',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+          style: TextButton.styleFrom(
+            shape: BeveledRectangleBorder(),
+            backgroundColor: Color.fromARGB(255, 75, 60, 60),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+      Text(
+        '12312312312312',
+        style: TextStyle(fontSize: 16),
+      ),
+    ];
+  }
+
+  List<Widget> createBottomDetailColum() {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('123123123'),
+          Expanded(
+            child: HorizontalLine(
+              dashedWidth: 500,
+              dashedHeight: 1,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      Container(
+        width: 570,
+        child: Text(
+            '測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字測試文字'),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      createPicBox(widget.item.image),
+      SizedBox(
+        height: 15,
+      ),
+      createPicBox(widget.item.image),
+      SizedBox(
+        height: 15,
+      ),
+      createPicBox(widget.item.image),
+    ];
+  }
+}
+
+SizedBox buildBtnWithColorBox(Color color) {
+  return SizedBox(
+    height: 15,
+    width: 15,
+    child: ElevatedButton(
+      onPressed: () {
+        print('pressssss');
+      },
+      style: ElevatedButton.styleFrom(backgroundColor: color),
+      child: Text(''),
     ),
   );
-}
 }
